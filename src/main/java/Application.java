4624513bb271acd8ccd0f0a0bc8fea8a37f5eefb
eman <emmanuel-adjei@outlook.com>
2021-08-27 14:51:34 +0100

    Spring used in main method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import service.SpeakerService;
 import service.SpeakerServiceImpl;
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+
+        ApplicationContext appCotext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = appCotext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstname());
         System.out.println(service.findAll().get(0).getLastName());
