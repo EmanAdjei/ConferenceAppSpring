f5238ba7445dff539fab27851a8060fb9c09cce6
eman <emmanuel-adjei@outlook.com>
2021-08-31 11:47:13 +0100

    Added FactoryBean tag

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,9 +1,25 @@
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import pluralsight.util.CalendarFactory;
+
+import java.util.Calendar;
 
 @Configuration
 @ComponentScan({"pluralsight"})
 public class AppConfig {
+
+    @Bean(name = "cal")
+    public CalendarFactory calFactory(){
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(3);
+        return factory;
+    }
+
+    @Bean
+    public Calendar cal() throws Exception{
+        return calFactory().getObject();
+    }
 
 //    @Bean(name = "speakerService")
 //    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
--- src/main/java/pluralsight/respository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/pluralsight/respository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,21 @@
 package pluralsight.respository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import pluralsight.model.Speaker;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
+
+    private Date calDate;
 
     public List<Speaker> findAll(){
 
@@ -18,11 +26,14 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setFirstname("John");
         speaker.setLastName("Doe");
 
+        calDate = cal.getTime();
+
+        //Gets printed twice, find all method called twice
+        System.out.println("cal: " + calDate);
+
         speakers.add(speaker);
 
         return speakers;
     }
-
-
 
 }
